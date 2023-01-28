ffa15366ab03b88b9382e7330603e5b63fe39b32
Mr Reid <64812253+sreid111@users.noreply.github.com>
2023-01-28 14:33:00 +1000

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+a = 0
+b = 2
+for index in range(4):
+    a += b
+basic.show_number(a)
---
+++ main.ts
@@ -0,0 +1,6 @@
+let a = 0
+let b = 2
+for (let index = 0; index < 4; index++) {
+    a += b
+}
+basic.showNumber(a)
